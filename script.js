1790b1b090b5503c463e2a27342d6383c4411c44
David Calaiò <davidcjr11@gmail.com>
2023-03-28 17:36:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const pUtente = prompt('Inserisci la parola chiave')
+const parola = 'Palindroma'
+
+
+const confermaParola = (parola === pUtente) ? alert('Buon divertimento') : alert('Mi dispiace ma non puoi giocare')   
+
+console.log(confermaParola)
+console.log(parola)
+console.log(pUtente)
+
